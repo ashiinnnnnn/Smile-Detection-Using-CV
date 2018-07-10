063ca79febee87e48ef7f2a326f9c5fde7726545
Aditya Joshi <aditya.joshi_ug20@ashoka.edu.in>
2018-07-11 03:57:27 +0545

    the file for smile detection

---
+++ SmileDetection/smileInterpreter.py
@@ -0,0 +1,35 @@
+#imported the library
+import cv2
+
+#assigning cascades to different variables as required
+face_cascade = cv2.CascadeClassifier('frontalface.xml')
+eye_cascade = cv2.CascadeClassifier('eye.xml')
+smile_cascade = cv2.CascadeClassifier('smile.xml')
+
+# the function below detects the eyes and smile
+def detect(gray, frame):
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 22)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+        smiles = smile_cascade.detectMultiScale(roi_gray, 1.7, 25)
+        for (sx, sy, sw, sh) in smiles:
+            cv2.rectangle(roi_color, (sx, sy), (sx+sw, sy+sh), (0, 0, 255), 2)
+    return frame
+
+# face recognition with webcam
+video_capture = cv2.VideoCapture(0)
+while True:
+    _, frame = video_capture.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    canvas = detect(gray, frame)
+    cv2.imshow('Video', canvas)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        # pressing 'q' will destroy the windows apart from the program window
+        break
+video_capture.release()
+cv2.destroyAllWindows()
